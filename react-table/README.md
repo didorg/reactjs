5ee4bacae92c8e18df1dc466dd6124a370913a8d
Didier <didorg@gmail.com>
2021-07-19 11:53:36 -0400

    React Table

---
+++ react-table/README.md
@@ -0,0 +1,4 @@
+# React Table
+
+### Videos  
+https://www.youtube.com/watch?v=YwP4NAZGskg&list=PLC3y8-rFHvwgWTSrDiwmUsl4ZvipOw9Cz&index=2&ab_channel=Codevolution
---
+++ react-table/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import { BasicTable } from './components/BasicTable'
+
+function App() {
+  return (
+    <div className='App'>
+      <BasicTable />
+    </div>
+  )
+}
+
+export default App

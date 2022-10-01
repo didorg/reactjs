32fcf2904eaf35291346fa0092ef1ce5b7d028fe
Didier <didorg@gmail.com>
2022-10-01 16:58:06 -0400

    [sec4]$Hooks

---
+++ course-sec4-hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Setup from "./tutorial/1-useState/setup/2-useState-basics";
+
+function App() {
+  return (
+    <div className="container">
+      <h2>Advanced Tutorial</h2>
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
